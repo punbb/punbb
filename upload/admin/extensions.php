cb8d19826c75305402563e2e5dbe95c50b4f8aa4
Anatoly <Anatoly@4dc816b1-62af-4023-b237-cae3452c1eb1>
2008-10-30 14:37:11 +0000

    Fixing an error message being displayed if your extension has no hooks. Imported from FluxBB.
    git-svn-id: http://punbb.informer.com/svn/punbb/branches/punbb-1.3-dev@746 4dc816b1-62af-4023-b237-cae3452c1eb1

--- upload/admin/extensions.php
+++ upload/admin/extensions.php
@@ -187,21 +187,24 @@ if (isset($_GET['install']) || isset($_GET['install_hotfix']))
 		}
 
 		// Now insert the hooks
-		foreach ($ext_data['extension']['hooks']['hook'] as $ext_hook)
+		if (isset($ext_data['extension']['hooks']['hook']))
 		{
-			$cur_hooks = explode(',', $ext_hook['attributes']['id']);
-			foreach ($cur_hooks as $cur_hook)
-			{
-				$query = array(
-					'INSERT'	=> 'id, extension_id, code, installed, priority',
-					'INTO'		=> 'extension_hooks',
-					'VALUES'	=> '\''.$forum_db->escape(forum_trim($cur_hook)).'\', \''.$forum_db->escape($id).'\', \''.$forum_db->escape(forum_trim($ext_hook['content'])).'\', '.time().', '.(isset($ext_hook['attributes']['priority']) ? $ext_hook['attributes']['priority'] : 5)
-				);
+			foreach ($ext_data['extension']['hooks']['hook'] as $ext_hook)
+  			{
+				$cur_hooks = explode(',', $ext_hook['attributes']['id']);
+				foreach ($cur_hooks as $cur_hook)
+				{
+					$query = array(
+						'INSERT'	=> 'id, extension_id, code, installed, priority',
+						'INTO'		=> 'extension_hooks',
+						'VALUES'	=> '\''.$forum_db->escape(forum_trim($cur_hook)).'\', \''.$forum_db->escape($id).'\', \''.$forum_db->escape(forum_trim($ext_hook['content'])).'\', '.time().', '.(isset($ext_hook['attributes']['priority']) ? $ext_hook['attributes']['priority'] : 5)
+					);
 
-				($hook = get_hook('aex_install_comply_qr_add_hook')) ? eval($hook) : null;
-				$forum_db->query_build($query) or error(__FILE__, __LINE__);
-			}
-		}
+					($hook = get_hook('aex_install_comply_qr_add_hook')) ? eval($hook) : null;
+					$forum_db->query_build($query) or error(__FILE__, __LINE__);
+				}
+  			}
+  		}
 
 		// Empty the PHP cache
 		forum_clear_cache();
