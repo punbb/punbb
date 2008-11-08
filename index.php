f1ec4eab5e7979f31c10590e462bd9f927665163
Anatoly <Anatoly@4dc816b1-62af-4023-b237-cae3452c1eb1>
2008-11-08 10:30:00 +0000

    Fixed "Board is empty" message in index.php.
    git-svn-id: http://punbb.informer.com/svn/punbb/branches/punbb-1.3-dev@827 4dc816b1-62af-4023-b237-cae3452c1eb1

--- index.php
+++ index.php
@@ -239,7 +239,10 @@ if ($forum_page['cur_category'] > 0)
 else
 {
 
-?>	<div class="main-content message">
+?>	<div class="main-head">
+		<h2 class="hn"><span><?php echo $lang_common['Forum message']?></span></h2>
+	</div>
+	<div class="main-content main-message">
 		<p><?php echo $lang_index['Empty board'] ?></p>
 	</div>
 <?php
