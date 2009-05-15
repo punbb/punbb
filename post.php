eddcee361924d98310b7f5788f478fe177ec45bd
Roman <Roman@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-05-15 11:25:04 +0000

    Fixed inverse numeration on post preview (reported by maststef). Removed unnecessary code and a hook in post.php.
    git-svn-id: http://punbb.informer.com/svn/punbb/trunk@1162 4dc816b1-62af-4023-b237-cae3452c1eb1

--- post.php
+++ post.php
@@ -548,13 +548,24 @@ if ($tid && $forum_config['o_topic_review'] != '0')
 	if (!defined('FORUM_PARSER_LOADED'))
 		require FORUM_ROOT.'include/parser.php';
 
+	// Get the amount of posts in the topic
+	$query = array(
+		'SELECT'	=> 'count(p.id)',
+		'FROM'		=> 'posts AS p',
+		'WHERE'		=> 'topic_id='.$tid
+	);
+
+	($hook = get_hook('po_topic_review_qr_get_post_count')) ? eval($hook) : null;
+	$result = $forum_db->query_build($query) or error(__FILE__, __LINE__);
+	$forum_page['total_post_count'] = $forum_db->result($result, 0);
+
 	// Get posts to display in topic review
 	$query = array(
 		'SELECT'	=> 'p.id, p.poster, p.message, p.hide_smilies, p.posted',
 		'FROM'		=> 'posts AS p',
 		'WHERE'		=> 'topic_id='.$tid,
-		'ORDER BY'	=>	'id DESC',
-		'LIMIT'		=>	$forum_config['o_topic_review']
+		'ORDER BY'	=> 'id DESC',
+		'LIMIT'		=> $forum_config['o_topic_review']
 	);
 
 	($hook = get_hook('po_topic_review_qr_get_topic_review_posts')) ? eval($hook) : null;
@@ -574,19 +585,11 @@ if ($tid && $forum_config['o_topic_review'] != '0')
 	{
 		++$forum_page['item_count'];
 
-		// Generate the post heading
-		$forum_page['post_ident'] = array(
-			'num'	=> '<span class="post-num">'.forum_number_format($forum_page['item_count']).'</span>',
-			'link'	=> '<span class="post-link">'.sprintf($lang_post['Post posted'], '<a class="permalink" rel="bookmark" title="'.$lang_post['Permalink post'].'" href="'.forum_link($forum_url['post'], $cur_post['id']).'">'.format_time($cur_post['posted']).'</a>').'</span>'
-		);
-
-		($hook = get_hook('po_topic_review_pre_item_indent_merge')) ? eval($hook) : null;
-
 		$forum_page['message'] = parse_message($cur_post['message'], $cur_post['hide_smilies']);
 
 		// Generate the post heading
 		$forum_page['post_ident'] = array();
-		$forum_page['post_ident']['num'] = '<span class="post-num">'.forum_number_format($forum_page['item_count']).'</span>';
+		$forum_page['post_ident']['num'] = '<span class="post-num">'.forum_number_format($forum_page['total_post_count'] - $forum_page['item_count'] + 1).'</span>';
 		$forum_page['post_ident']['byline'] = '<span class="post-byline">'.sprintf($lang_post['Post byline'], '<strong>'.forum_htmlencode($cur_post['poster']).'</strong>').'</span>';
 		$forum_page['post_ident']['link'] = '<span class="post-link"><a class="permalink" rel="bookmark" title="'.$lang_post['Permalink post'].'" href="'.forum_link($forum_url['post'], $cur_post['id']).'">'.format_time($cur_post['posted']).'</a></span>';
 
