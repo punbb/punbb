0fa1b380b1dedea0a30a4450057c96ba9c6bb3ec
SlavaK <SlavaK@4dc816b1-62af-4023-b237-cae3452c1eb1>
2009-09-17 14:12:08 +0000

    Added new extension "pun_broadcast_email".
    git-svn-id: http://punbb.informer.com/svn/punbb/trunk@1338 4dc816b1-62af-4023-b237-cae3452c1eb1

---
+++ extensions/pun_broadcast_email/lang/English/pun_broadcast_email.php
@@ -0,0 +1,40 @@
+<?php
+
+if (!defined('FORUM'))
+	exit;
+
+$lang_pun_broadcast_email = array(
+	'Email errors'	=>	'<strong>Warning!</strong> The following errors must be corrected before sending e-mails:',
+	'Ext name'		=>	'Broadcast e-mail',
+	'Group'			=>	'Group',
+	'Group title'	=>	'Group title',
+	'Members count'	=>	'Members count',
+	'Tpl vars'		=>	'Template vars',
+	'Tpl vars info'	=>	'Enable this option if you are planning to use <a class="exthelp" href="%s">special vars</a> in the e-mail template.',
+	'Email subject'	=>	'E-mail subject',
+	'Email message'	=>	'E-mail message',
+	'Submit'		=>	'Submit',
+	'Preview'		=>	'Preview',
+	'Ext help'		=>	'Broadcast e-mail help',
+	'Ext help header'=>	'The list of special template variables which you can use in e-mails.',
+	'Err no groups'		=>	'No groups selected.',
+	'Err guest group'	=>	'You can\'t send e-mail to guest users.',
+	'Err no subject'	=>	'No e-mail subject.',
+	'Err long subject'	=>	'The subject is too long',
+	'Err no message'	=>	'No message',
+	'Err long message'	=>	'The length of your e-mail message is %s bytes. This exceeds the %s bytes limit.',
+	'Email sent'		=>	'E-mail message has been sent.',
+	'Table summary'		=>	'Groups',
+	'Help username'		=>	'produces username.',
+	'Help user title'	=>	'produces user title.',
+	'Help realname'		=>	'produces realname.',
+	'Help num posts'	=>	'produces number of user posts.',
+	'Help last post'	=>	'produces date of the last post of the user.',
+	'Help reg date'		=>	'produces registration date of the user.',
+	'Help reg IP'		=>	'produces the IP which was used by the user for registration.',
+	'Help last visit'	=>	'produces date of the last visit of the user.',
+	'Help admin note'	=>	'produces admin note for the user.',
+	'Help user profile'		=>	'produces url to the user profile.',
+);
+
+?>
