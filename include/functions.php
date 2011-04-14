375d381a64fb5a286f996d9fb77b44cb866e6cd6
dimkalinux <dimka.linux@gmail.com>
2011-04-14 11:44:02 +0300

    Add comment for forum_microtime() function

--- include/functions.php
+++ include/functions.php
@@ -50,7 +50,7 @@ function check_is_all_caps($text)
 }
 
 
-//
+// Return current timestamp (with microseconds) as a float
 function forum_microtime()
 {
 	if (version_compare(PHP_VERSION, '5.0.0', '>='))
