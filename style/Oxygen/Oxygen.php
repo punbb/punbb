b881e84b7d945bde35a9bd29227f5d4031c17a2c
dimkalinux <dimka.linux@gmail.com>
2011-04-30 22:35:27 +0300

    Fix CSS load in Oxygen style

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -2,8 +2,8 @@
 
 // $forum_loader->add_css('style/Oxygen/min/Oxygen.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
-$forum_loader->add_css(FORUM_ROOT.'style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
-$forum_loader->add_css(FORUM_ROOT.'style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css('style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css('style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 
 // IE
