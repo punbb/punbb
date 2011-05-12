694bf406e4c896583082e88cdde196bf9ecf13c4
dimkalinux <dimka.linux@gmail.com>
2011-05-12 13:56:38 +0300

    Add media styles for iphone

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -3,6 +3,7 @@
 // $forum_loader->add_css('style/Oxygen/min/Oxygen.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 $forum_loader->add_css('style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css('style/Oxygen/Oxygen_mobile.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 $forum_loader->add_css('style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 
