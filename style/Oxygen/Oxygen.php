7a69c7cef496921deca239269449c997256640a9
dimkalinux <dimka.linux@gmail.com>
2011-06-24 10:57:03 +0300

    Oxygen styles: move all IE-related  rules to Oxygen.css.

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -5,10 +5,4 @@
 $forum_loader->add_css('style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 $forum_loader->add_css('style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
-
-// IE
-$forum_loader->add_css('style/Oxygen/min/Oxygen_ie6.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen', 'browsers' => array('IE' => 'lte IE 6', '!IE' => false)));
-$forum_loader->add_css('style/Oxygen/min/Oxygen_ie7.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen', 'browsers' => array('IE' => 'IE 7', '!IE' => false)));
-$forum_loader->add_css('style/Oxygen/min/Oxygen_ie8.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen', 'browsers' => array('IE' => 'IE 8', '!IE' => false)));
-
 ?>
