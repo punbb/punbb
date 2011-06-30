2368bfb8e2095f17ba5feb4ed5f19bc0fabd930b
dimkalinux <dimka.linux@gmail.com>
2011-06-30 18:41:00 +0300

    Oxygen.php: switch to minified version of CSS

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -1,8 +1,8 @@
 <?php
 
-// $forum_loader->add_css('style/Oxygen/min/Oxygen.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css('style/Oxygen/min/Oxygen.min.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
-$forum_loader->add_css('style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
-$forum_loader->add_css('style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+//$forum_loader->add_css('style/Oxygen/Oxygen.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+//$forum_loader->add_css('style/Oxygen/Oxygen_cs.css', array('type' => 'file', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 ?>
