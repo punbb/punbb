9a105f9fda07113f0defc35d87f5bb8effe665ec
dimkalinux <dimka.linux@gmail.com>
2011-07-10 20:22:43 +0300

    Switch to productions styles.

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -1,8 +1,5 @@
 <?php
 
-//$forum_loader->add_css($base_url.'/style/Oxygen/min/Oxygen.min.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
-
-$forum_loader->add_css($base_url.'/style/Oxygen/Oxygen.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
-$forum_loader->add_css($base_url.'/style/Oxygen/Oxygen_cs.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css($base_url.'/style/Oxygen/min/Oxygen.min.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 ?>
