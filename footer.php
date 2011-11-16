48634c868513b1426a6072de0fcf8cd2ab473ade
dimkalinux <dimka.linux@gmail.com>
2011-11-16 21:32:24 +0200

    Footer: fix IE7 javascript errors

--- footer.php
+++ footer.php
@@ -108,7 +108,7 @@ $forum_javascript_commonjs_urls = '
 		user_lang: "'.forum_htmlencode($forum_user['language']).'",
 		user_style: "'.forum_htmlencode($forum_user['style']).'",
 		user_is_guest: "'.forum_htmlencode(($forum_user['is_guest'] == 1) ? "1" : "0").'",
-		page: "'.forum_htmlencode((defined("FORUM_PAGE")) ? FORUM_PAGE : "unknown" ).'",
+		page: "'.forum_htmlencode((defined("FORUM_PAGE")) ? FORUM_PAGE : "unknown" ).'"
 	};';
 
 
