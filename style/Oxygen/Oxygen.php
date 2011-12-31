16e89770b64c175edcf337e5ca211d7b67b34aab
dimkalinux <dimka.linux@gmail.com>
2011-12-31 17:34:37 +0200

    Oxygen: move minified files from min/ dir to style dir

--- style/Oxygen/Oxygen.php
+++ style/Oxygen/Oxygen.php
@@ -1,5 +1,5 @@
 <?php
 
-$forum_loader->add_css($base_url.'/style/Oxygen/min/Oxygen.min.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
+$forum_loader->add_css($base_url.'/style/Oxygen/Oxygen.min.css', array('type' => 'url', 'group' => FORUM_CSS_GROUP_SYSTEM, 'media' => 'screen'));
 
 ?>
