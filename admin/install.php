7d398fc6d9739d85cd982c0bb46e4a2bf97b6ec6
dimkalinux <dimka.linux@gmail.com>
2012-01-07 21:16:56 +0200

    Update version to 1.4.2RC1

--- admin/install.php
+++ admin/install.php
@@ -10,7 +10,7 @@
  */
 
 
-define('FORUM_VERSION', '1.4.1');
+define('FORUM_VERSION', '1.4.2RC1');
 define('FORUM_DB_REVISION', 5);
 define('MIN_PHP_VERSION', '5.0.0');
 define('MIN_MYSQL_VERSION', '4.1.2');
