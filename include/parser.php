cfcfd2f227421a61f5e0afe45f48a54905017638
keeshii <keeshii@ptcg.eu>
2014-09-17 22:25:40 +0200

    anonymouse function replaced by create_function to improve compability.

--- include/parser.php
+++ include/parser.php
@@ -742,12 +742,11 @@ function do_bbcode($text, $is_signature = false)
 
 	if (strpos($text, '[quote') !== false)
 	{
-		$text = preg_replace_callback(
+		$text = preg_replace_callback (
 			'#\[quote=(&\#039;|&quot;|"|\'|)(.*?)\\1\]#',
-			function ($matches) {
-				global $lang_common;
-				return "</p><div class=\"quotebox\"><cite>".str_replace(array('[', '\"'), array('&#91;', '"'), $matches[2])." ".$lang_common['wrote'].":</cite><blockquote><p>";
-			}, $text);
+			create_function ('$matches', 'global $lang_common; return "</p><div class=\"quotebox\"><cite>".str_replace(array(\'[\', \'\"\'), array(\'&#91;\', \'"\'), $matches[2])." ".$lang_common[\'wrote\'].":</cite><blockquote><p>";'),
+			$text
+		);
 		$text = preg_replace('#\[quote\]\s*#', '</p><div class="quotebox"><blockquote><p>', $text);
 		$text = preg_replace('#\s*\[\/quote\]#S', '</p></blockquote></div><p>', $text);
 	}
