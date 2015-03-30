0993735be1f6819c1c85c31766aff30f6362df68
Rick Deckard <xxxblasteroid@gmail.com>
2015-03-30 15:29:20 +0300

    split-to-chunks

--- userlist.php
+++ userlist.php
@@ -118,12 +118,11 @@ define('FORUM_ALLOW_INDEX', 1);
 define('FORUM_PAGE', 'userlist');
 require FORUM_ROOT.'header.php';
 
-// START SUBST - <!-- forum_main -->
+$view_forum_main = 'userlist/main';
+
 ob_start();
-include FORUM_ROOT . 'include/view/userlist/main.php';
-$view_forum_main = forum_trim(ob_get_contents());
-$tpl_main = str_replace('<!-- forum_main -->', $view_forum_main, $tpl_main);
+include view($view_forum_layout);
+$tpl_main = forum_trim(ob_get_contents());
 ob_end_clean();
-// END SUBST - <!-- forum_main -->
 
 require FORUM_ROOT.'footer.php';
