07ad8dd1e9a0f6b6790cc3f80a2f2b5117838cdd
Rick Deckard <xxxblasteroid@gmail.com>
2015-03-30 12:32:15 +0300

    split-to-chunks

--- edit.php
+++ edit.php
@@ -206,12 +206,11 @@ $forum_page['crumbs'] = array(
 define('FORUM_PAGE', 'postedit');
 require FORUM_ROOT.'header.php';
 
-// START SUBST - <!-- forum_main -->
+$view_forum_main = 'edit/main';
+
 ob_start();
-include FORUM_ROOT . 'include/view/edit/main.php';
-$view_forum_main = forum_trim(ob_get_contents());
-$tpl_main = str_replace('<!-- forum_main -->', $view_forum_main, $tpl_main);
+include view($view_forum_layout);
+$tpl_main = forum_trim(ob_get_contents());
 ob_end_clean();
-// END SUBST - <!-- forum_main -->
 
 require FORUM_ROOT.'footer.php';
