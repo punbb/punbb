abd862aed44b93463d05ac7fb3dfe289f28db198
Rick Deckard <xxxblasteroid@gmail.com>
2015-03-31 08:22:35 +0300

    render-admin-pages

--- admin/reindex.php
+++ admin/reindex.php
@@ -200,14 +200,6 @@ $forum_page['crumbs'] = array(
 
 define('FORUM_PAGE_SECTION', 'management');
 define('FORUM_PAGE', 'admin-reindex');
-require FORUM_ROOT.'header.php';
 
-// START SUBST - <!-- forum_main -->
-ob_start();
-include FORUM_ROOT . 'include/view/admin/reindex/main.php';
-$view_forum_main = forum_trim(ob_get_contents());
-$tpl_main = str_replace('<!-- forum_main -->', $view_forum_main, $tpl_main);
-ob_end_clean();
-// END SUBST - <!-- forum_main -->
-
-require FORUM_ROOT.'footer.php';
+$view_forum_main = 'admin/reindex/main';
+include FORUM_ROOT . 'include/render.php';
