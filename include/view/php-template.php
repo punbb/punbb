ac0b19f3b45e0bf9cbfe8bd1d4d007dc6922e7d0
Rick Deckard <xxxblasteroid@gmail.com>
2015-04-01 11:33:38 +0300

    overriding tempates in user theme

--- include/view/php-template.php
+++ include/view/php-template.php
@@ -1,12 +1,30 @@
 <?php
 
 function view($name) {
+	global $forum_user;
+
 	if ($name == '') {
 		$name = 'blank';
 	}
-	return FORUM_ROOT . 'include/view/' . $name . '.php';
+	// use from style folder
+	$fname = FORUM_ROOT . 'style/' . $forum_user['style'] . '/' . $name . '.php';
+	if (!file_exists($fname)) {
+		// use default
+		$fname =  FORUM_ROOT . 'include/view/' . $name . '.php';
+	}
+
+	return $fname;
 }
 
 function helper($name) {
-	return include FORUM_ROOT . 'include/view/helper/' . $name . '.php';
+	global $forum_user;
+
+	// use from style folder
+	$fname = FORUM_ROOT . 'style/' . $forum_user['style'] . '/helper/' . $name . '.php';
+	if (!file_exists($fname)) {
+		// use default
+		$fname = FORUM_ROOT . 'include/view/helper/' . $name . '.php';
+	}
+
+	return include $fname;
 }
