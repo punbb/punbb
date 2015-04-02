4f5c0a1e3cb3099fc27823e25b27f95303ef827d
Rick Deckard <xxxblasteroid@gmail.com>
2015-04-02 16:11:13 +0300

    fix-style-render

--- include/render.php
+++ include/render.php
@@ -2,12 +2,6 @@
 
 require FORUM_ROOT . 'header.php';
 
-// init theme
-$_tmp_fname = FORUM_ROOT . 'style/' . $forum_user['style'] . '/init.php';
-if (file_exists($_tmp_fname)) {
-	include $_tmp_fname;
-}
-
 // render view
 $_tmp_fname = FORUM_ROOT . 'style/' . $forum_user['style'] . '/render.php';
 if (file_exists($_tmp_fname)) {
