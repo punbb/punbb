886f2eb4f87068c3de49f05ba5d3e8eb796518aa
Rick Deckard <xxxblasteroid@gmail.com>
2015-04-03 10:07:32 +0300

    fix-edit-template

--- include/view/edit/main.php
+++ include/view/edit/main.php
@@ -6,49 +6,9 @@
 	<div class="main-head">
 		<h2 class="hn"><span><?php echo ($id == $cur_post['first_post_id']) ? $lang_post['Edit topic'] : $lang_post['Edit reply'] ?></span></h2>
 	</div>
-<?php
 
-// If preview selected and there are no errors
-if (isset($_POST['preview']) && empty($forum_page['errors']))
-{
-	if (!defined('FORUM_PARSER_LOADED'))
-		require FORUM_ROOT.'include/parser.php';
+	<?php include view('edit/preview') ?>
 
-	// Generate the post heading
-	$forum_page['post_ident'] = array();
-	$forum_page['post_ident']['num'] = '<span class="post-num">#</span>';
-	$forum_page['post_ident']['byline'] = '<span class="post-byline">'.sprintf((($id == $cur_post['first_post_id']) ? $lang_post['Topic byline'] : $lang_post['Reply byline']), '<strong>'.forum_htmlencode($cur_post['poster']).'</strong>').'</span>';
-	$forum_page['post_ident']['link'] = '<span class="post-link">'.format_time(time()).'</span>';
-
-	$forum_page['preview_message'] = parse_message($message, $hide_smilies);
-
-	($hook = get_hook('ed_preview_pre_display')) ? eval($hook) : null;
-
-?>
-	<div class="main-subhead">
-		<h2 class="hn"><span><?php echo $id == $cur_post['first_post_id'] ? $lang_post['Preview edited topic'] : $lang_post['Preview edited reply'] ?></span></h2>
-	</div>
-	<div id="post-preview" class="main-content main-frm">
-		<div class="post singlepost">
-			<div class="posthead">
-				<h3 class="hn"><?php echo implode(' ', $forum_page['post_ident']) ?></h3>
-<?php ($hook = get_hook('ed_preview_new_post_head_option')) ? eval($hook) : null; ?>
-			</div>
-			<div class="postbody">
-				<div class="post-entry">
-					<div class="entry-content">
-						<?php echo $forum_page['preview_message']."\n" ?>
-					</div>
-<?php ($hook = get_hook('ed_preview_new_post_entry_data')) ? eval($hook) : null; ?>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php
-
-}
-
-?>
 	<div class="main-subhead">
 		<h2 class="hn"><span><?php echo ($id != $cur_post['first_post_id']) ? $lang_post['Compose edited reply'] : $lang_post['Compose edited topic'] ?></span></h2>
 	</div>
@@ -58,21 +18,7 @@ if (isset($_POST['preview']) && empty($forum_page['errors']))
 	if (!empty($forum_page['text_options']))
 		echo "\t\t".'<p class="ct-options options">'.sprintf($lang_common['You may use'], implode(' ', $forum_page['text_options'])).'</p>'."\n";
 
-// If there were any errors, show them
-if (isset($forum_page['errors']))
-{
-
-?>
-		<div class="ct-box error-box">
-			<h2 class="warn hn"><span><?php echo $lang_post['Post errors'] ?></span></h2>
-			<ul class="error-list">
-				<?php echo implode("\n\t\t\t\t", $forum_page['errors'])."\n" ?>
-			</ul>
-		</div>
-<?php
-
-}
-
+	include view('edit/errors');
 ?>
 		<div id="req-msg" class="req-warn ct-box error-box">
 			<p class="important"><?php echo $lang_common['Required warn'] ?></p>
