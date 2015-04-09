5d2f19598de115bb9a545a1b619e4b5f36364d41
Rick Deckard <xxxblasteroid@gmail.com>
2015-04-09 10:13:23 +0300

    fix-login-vietopic

--- include/view/viewtopic/main.php
+++ include/view/viewtopic/main.php
@@ -1,4 +1,5 @@
 <?php
+namespace punbb;
 
 ($hook = get_hook('vt_main_output_start')) ? eval($hook) : null;
 
--- include/view/viewtopic/qpost.php
+++ include/view/viewtopic/qpost.php
@@ -1,4 +1,5 @@
 <?php
+namespace punbb;
 
 ($hook = get_hook('vt_qpost_output_start')) ? eval($hook) : null;
 
