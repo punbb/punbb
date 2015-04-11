1e8bf909a3109deee9879fe9c3d6ede0ab48fefa
Rick Deckard <xxxblasteroid@gmail.com>
2015-04-11 09:15:01 +0300

    test

--- include/services.php
+++ include/services.php
@@ -111,3 +111,29 @@ user(function () {
 	}
 	return $_PUNBB['user'];
 });
+
+/*
+function test_object($serv = null) {
+	return service('test_object', $serv);
+}
+
+test_object(function () {
+	global $_PUNBB;
+
+	if (!isset($_PUNBB['test_object'])) {
+		$_PUNBB['test_object'] = new \stdClass();
+	}
+
+	return $_PUNBB['test_object'];
+});
+
+echo '<pre>';
+print_r(test_object());
+
+test_object()->parameter1 = 200;
+test_object()->parameter5 = 500;
+test_object()->test = rand(1, 100);
+
+print_r(test_object());
+die;
+*/
