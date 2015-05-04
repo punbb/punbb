bdb4345f34f5dad6bb2d7a0d46123930a97bfc2d
Rick Deckard <xxxblasteroid@gmail.com>
2015-05-04 15:18:35 +0300

    fix

--- include/view/helper/announcement.php
+++ include/view/helper/announcement.php
@@ -1,13 +1,16 @@
 <?php
 namespace punbb;
 
-if (config()->o_announcement == '1' && user()->g_read_board == '1') { ?>
+$config = config();
+$user = user();
+
+if ($config->o_announcement == '1' && $user->g_read_board == '1') { ?>
 	<div id="brd-announcement" class="gen-content">
-		<?php if (config()->o_announcement_heading != '') { ?>
-			<h1 class="hn"><span><?= config()->o_announcement_heading ?></span></h1>
+		<?php if ($config->o_announcement_heading != '') { ?>
+			<h1 class="hn"><span><?= $config->o_announcement_heading ?></span></h1>
 		<?php } ?>
 		<div class="content">
-			<?= config()->o_announcement_message ?>
+			<?= $config->o_announcement_message ?>
 		</div>
 	</div>
 <?php } ?>
