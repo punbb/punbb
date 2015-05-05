e7754ce7d5ef5ba2aa2ec6211433803f4d53e946
Rick Deckard <xxxblasteroid@gmail.com>
2015-05-05 10:17:18 +0300

    fix

--- include/view/partial/about.php
+++ include/view/partial/about.php
@@ -1,30 +1,27 @@
 <?php
 namespace punbb;
 
-($hook = get_hook('ft_about_output_start')) ? eval($hook) : null;
+$config = config();
+$user = user();
 
 // Display the "Jump to" drop list
-if (user()->g_read_board == '1' && config()->o_quickjump == '1') {
-	($hook = get_hook('ft_about_pre_quickjump')) ? eval($hook) : null;
-
+if ($user->g_read_board == '1' && $config->o_quickjump == '1') {
 	// Load cached quickjump
-	if (file_exists(FORUM_CACHE_DIR.'cache_quickjump_'.user()->g_id.'.php')) {
-		include FORUM_CACHE_DIR.'cache_quickjump_'.user()->g_id.'.php';
+	if (file_exists(FORUM_CACHE_DIR . 'cache_quickjump_' . $user->g_id . '.php')) {
+		include FORUM_CACHE_DIR . 'cache_quickjump_' . $user->g_id . '.php';
 	}
 
 	if (!defined('FORUM_QJ_LOADED')) {
 		require FORUM_ROOT . 'include/cache.php';
-		generate_quickjump_cache(user()->g_id);
-		require FORUM_CACHE_DIR . 'cache_quickjump_'.user()->g_id.'.php';
+		generate_quickjump_cache($user->g_id);
+		require FORUM_CACHE_DIR . 'cache_quickjump_' . $user->g_id . '.php';
 	}
 }
 
-($hook = get_hook('ft_about_pre_copyright')) ? eval($hook) : null;
-
 ?>
 	<p id="copyright"><?= sprintf(__('Powered by'),
-		'<a href="http://punbb.informer.com/">PunBB</a>'.(config()->o_show_version == '1' ? ' '.
-		config()->o_cur_version : ''), '<a href="http://www.informer.com/">Informer Technologies, Inc</a>') ?></p>
+		'<a href="http://punbb.informer.com/">PunBB</a>' .
+		($config->o_show_version == '1'? (' ' . $config->o_cur_version) : ''),
+		'<a href="http://www.informer.com/">Informer Technologies, Inc</a>') ?>
+	</p>
 <?php
-
-($hook = get_hook('ft_about_end')) ? eval($hook) : null;
