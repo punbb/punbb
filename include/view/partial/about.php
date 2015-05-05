17a1be7c75d035fe650e530d6e06faa7bdf16df8
Rick Deckard <xxxblasteroid@gmail.com>
2015-05-05 12:48:36 +0300

    cache

--- include/view/partial/about.php
+++ include/view/partial/about.php
@@ -6,7 +6,6 @@ $user = user();
 
 // Display the "Jump to" drop list
 if ($user->g_read_board == '1' && $config->o_quickjump == '1') {
-	// Load cached quickjump
 	$cached = cache()->get('cache_quickjump_' . $user->g_id);
 	if (!$cached) {
 		cache()->generate('quickjump_cache', $user->g_id);
