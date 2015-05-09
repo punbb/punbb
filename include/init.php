9a18e46fa603a6ac7b13c8dc4aeb97d9bc3d5e11
Rick Deckard <xxxblasteroid@gmail.com>
2015-05-09 17:40:40 +0300

    fix

--- include/init.php
+++ include/init.php
@@ -1,65 +1,67 @@
 <?php
 namespace punbb;
 
+use service;
+
 function path2url($path) {
 	return FORUM_WEB . substr($path, strlen(FORUM_ROOT));
 }
 
 function app() {
-	return \service::get('app');
+	return service::get('app');
 }
 
 function flash() {
-	return \service::get('flash');
+	return service::get('flash');
 }
 
 function db() {
-	return \service::get('db');
+	return service::get('db');
 }
 
 function config() {
-	return \service::get('config');
+	return service::get('config');
 }
 
 function user() {
-	return \service::get('user');
+	return service::get('user');
 }
 
 function assets() {
-	return \service::get('assets');
+	return service::get('assets');
 }
 
 function template() {
-	return \service::get('template');
+	return service::get('template');
 }
 
 function theme() {
-	return \service::get('theme');
+	return service::get('theme');
 }
 
 function language() {
-	return \service::get('language');
+	return service::get('language');
 }
 
 function translations() {
-	return \service::get('translations');
+	return service::get('translations');
 }
 
 function rewrite() {
-	return \service::get('rewrite');
+	return service::get('rewrite');
 }
 
 function cache() {
-	return \service::get('cache');
+	return service::get('cache');
 }
 
 function bbcode() {
-	return \service::get('bbcode');
+	return service::get('bbcode');
 }
 
 // configure
 
-\service::set('app', function () {
+service::set('app', function () {
 	$app = (object)require FORUM_ROOT . 'include/config/application.php';
 
 	// Record the start time (will be used to calculate the generation time for the page)
@@ -69,25 +71,25 @@ function bbcode() {
 	return $app;
 });
 
-\service::set('cache', function () {
+service::set('cache', function () {
 	return new Cache();
 });
 
-\service::set('config', function () {
+service::set('config', function () {
 	// Load cached config
 	$config = cache()->get('cache_config', 'punbb\\fn::generate_config_cache');
 	return (object)$config;
 });
 
-\service::set('flash', function () {
+service::set('flash', function () {
 	return new FlashMessenger();
 });
 
-\service::set('assets', function () {
+service::set('assets', function () {
 	return Loader::singleton();
 });
 
-\service::set('user', function () {
+service::set('user', function () {
 	// TODO fix
 	global $_PUNBB;
 	if (!isset($_PUNBB['user'])) {
@@ -98,10 +100,10 @@ function bbcode() {
 	return $_PUNBB['user'];
 });
 
-\service::set('translations', function () {
+service::set('translations', function () {
 	return new \stdClass();
 });
 
-\service::set('bbcode', function () {
+service::set('bbcode', function () {
 	return new BbcodeParser();
 });
